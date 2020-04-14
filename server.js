ecdb818e8c5d51327f442aa805b990bbdab45ac9
Jsegel <segeljosh@gmail.com>
2020-04-14 12:34:25 -0500

    adding initial files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+var path = require("path")
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// var routes = require("");
+// app.use(routes);
+app.listen(PORT, function(){
+    console.log("Server listening on http://localhost:" + PORT);
+}); 
